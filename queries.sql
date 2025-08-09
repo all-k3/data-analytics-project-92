361351357197f1ad0643d8f5e0a1c5979de8dc35
Alina <alina.kmkv23@gmail.com>
2025-08-09 18:16:33 +0300

    queries.sql

--- queries.sql
+++ queries.sql
@@ -66,3 +66,63 @@ order by
 	    when extract(dow from s.sale_date) = 0 then 7 
 	    else extract(dow from s.sale_date) 
     end;
+
+--этот запрос вычисляет количество покупателей в разных возрастных группах
+
+select 
+    '16-25' AS age_category,
+    count(customer_id) as age_count
+from 
+    customers 
+where age between 16 and 25
+
+union all
+
+select
+    '26-40' AS age_category,
+    count(customer_id) as age_count
+from 
+    customers 
+where age between 26 and 40
+
+union all  
+
+select 
+    '40+' AS age_category,
+   count(customer_id) as age_count
+from 
+    customers 
+where age > 40;
+
+--этот запрос предоставляет данные по количеству уникальных покупателей и выручке, которую они принесли 
+
+select 
+    to_char(sale_date, 'YYYY-MM') as selling_month,
+    COUNT(distinct customer_id) as total_customers,
+    SUM(quantity*p.price) as income
+from sales s
+left join 
+    products p on s.product_id = p.product_id
+group by 
+    selling_month 
+order by 
+    selling_month;
+
+--этот запрос предоставляет данные о покупателях, совершивших первую покупку в ходе проведения акций
+      
+select distinct on (s.customer_id)
+    concat(c.first_name, ' ', c.last_name) as customer,
+    s.sale_date,
+    concat(e.first_name, ' ', e.last_name) as seller
+from 
+    sales s
+join 
+    customers c on s.customer_id = c.customer_id
+join 
+    employees e on s.sales_person_id = e.employee_id
+join 
+    products p on s.product_id = p.product_id
+where 
+    p.price = 0
+order by 
+    s.customer_id, s.sale_date;
