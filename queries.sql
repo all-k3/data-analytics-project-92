233175646b0638935371f553950032b542a98431
Alina <alina.kmkv23@gmail.com>
2025-08-31 21:29:34 +0300

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -132,23 +132,26 @@ WITH ranked_sales AS (
         s.sale_date,
         CONCAT(c.first_name, ' ', c.last_name) AS customer,
         CONCAT(e.first_name, ' ', e.last_name) AS seller,
-        ROW_NUMBER() OVER (PARTITION BY s.customer_id ORDER BY s.sale_date) AS rn
-    FROM 
+        ROW_NUMBER() OVER (
+            PARTITION BY s.customer_id 
+            ORDER BY s.sale_date
+        ) AS rn
+    FROM
         sales AS s
-    JOIN 
+    JOIN
         customers AS c ON s.customer_id = c.customer_id
-    JOIN 
+    JOIN
         employees AS e ON s.sales_person_id = e.employee_id
-    JOIN 
+    JOIN
         products AS p ON s.product_id = p.product_id
-    WHERE 
+    WHERE
         p.price = 0
 )
 SELECT
     sale_date,
     customer,
     seller
-FROM 
+FROM
     ranked_sales
-WHERE 
+WHERE
     rn = 1;
